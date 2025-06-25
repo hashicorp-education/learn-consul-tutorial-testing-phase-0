0bb18678d91b9e4dd86bb38929659cfb45ec8408
danielehc <daniele@hashicorp.com>
2025-06-25 13:34:10 +0200

    Codebuild test

--- self-managed/infrastructure/docker/images/build_images.sh
+++ self-managed/infrastructure/docker/images/build_images.sh
@@ -86,23 +86,14 @@ IMAGE_TAG="${DOCKER_REPOSITORY}/${DOCKER_BASE_IMAGE}:latest"
 LATEST_TAG="-t ${DOCKER_REPOSITORY}/${DOCKER_BASE_IMAGE}:latest"
 LS_TAG="-t ${DOCKER_REPOSITORY}/${DOCKER_BASE_IMAGE}:${DOCKER_REPOSITORY}"
 
-set -x 
-
-docker build -t learn-consul-vms/base-image:latest -t learn-consul-vms/base-image:latest -t learn-consul-vms/base-image:learn-consul-vms .
-
-
 ## Build Docker image
 DOCKER_BUILDKIT=1 docker build \
   -t "${IMAGE_TAG}" ${LATEST_TAG} ${LS_TAG} . > /dev/null 2>&1
-
-
 
 if [ $? != 0 ]; then
   ts_log "\033[1m\033[31m[ERROR]\033[0m - Failed build for ${DOCKER_REPOSITORY}/${DOCKER_BASE_IMAGE}:v${CONSUL_TAG_VERSION}...exiting."
   exit 1
 fi
-
-set +x 
 
 ## Exit Image folder
 popd > /dev/null 2>&1
